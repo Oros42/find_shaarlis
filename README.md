6f0ab31033940e70d27fae379c3989996be04165
Oros42 <oros@ecirtam.net>
2014-11-30 18:05:26 +0100

    Ajout la génération de la liste des shaarlis

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Où sont les shaarlis ?
-=============
-
+======================
+  
 Fichier JSON qui liste les annuaires de shaarlis.  
 https://raw.githubusercontent.com/Oros42/find_shaarlis/master/annuaires.json  
 ou  
 https://ecirtam.net/find_shaarlis/annuaires.json  
-
+  
 Format
 ======
 ```
@@ -17,6 +17,15 @@ Format
 URL : URL de l'annuaire  
 TYPE : river | river-api | shaarlo | shaarlimages | shaarli-tv | annuaire  
 IS_ACTIVE : 0 | 1  
+  
+Générer la liste des shaarlis
+=============================
+```
+php find.php
+```
+Cela va créer 2 fichiers dans ./out/ avec la liste de tout les shaarlis trouvés :  
+- shaarlis.json  
+- shaarlis.opml  
   
 Ajout, modification ou suppression ?
 ====================================
---
+++ find.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * @author Oros <shaarli [ at ] ecirtam.net>
+ * @license public domain
+ */
+
+if(!is_file("config.php")){
+	copy("config.php.dist", "config.php");
+}
+
+include "config.php";
+
+if(!is_dir($output_folder)){
+	mkdir($output_folder);
+}
+
+$shaarlis = array();
+
+function clean_xmlUrl($url){
+	return preg_replace('/([^:]{1})\/\//', '$1/', str_replace("index.php", "", str_replace("php/?", "php?", $url)));
+}
+
+function clean_htmlUrl($url){
+	$url=clean_xmlUrl($url);
+	$p=strrpos($url, "/");
+	if($p > 9){
+		// https:/www.ecirtam.net/links/?do=rss
+		// -> https:/www.ecirtam.net/links/
+		return substr($url, 0, $p+1);
+	}else{
+		return $url;
+	}
+}
+
+/**
+ * Add shaarlis in $shaarlis
+ * @param URL of an OPML file
+ */
+function addViaOpml($URL) {
+	global $shaarlis;
+	$body = @file_get_contents($URL);
+	if(!empty($body)) {
+		$xml = @simplexml_load_string($body);
+		foreach ($xml->body->outline as $value) {
+			$attributes = $value->attributes();
+			$shaarlis[clean_xmlUrl((string)$attributes->xmlUrl)] = array('text'=> (string)$attributes->text, 'htmlUrl'=> clean_htmlUrl((string)$attributes->htmlUrl));
+		}
+	}
+}
+
+/**
+ * Create OPML file with all shaarlis
+ * Code from: https://github.com/mknexen/shaarli-api/blob/master/class/AbstractApiController.php
+ * and https://github.com/pfeff/opml.php
+ */
+function outputOPML() {
+	global $shaarlis_opml;
+	global $shaarlis;
+	$xml = new XMLWriter();
+	$xml->openURI($shaarlis_opml);
+	$xml->startDocument('1.0', 'UTF-8');
+		$xml->startElement('opml');
+			$xml->writeAttribute('version', '2.0');
+			$xml->startElement('head');
+			$xml->writeElement('title', 'Shaarli API OPML');
+			$xml->writeElement('dateModified', date("D, d M Y H:i:s T"));
+			$xml->endElement();
+			$xml->startElement('body');
+				foreach ($shaarlis as $xmlUrl =>$feed) {
+					$xml->startElement('outline');
+					$xml->writeAttribute('text', $feed['text']);
+					$xml->writeAttribute('htmlUrl', $feed['htmlUrl']);
+					$xml->writeAttribute('xmlUrl', $xmlUrl);
+					$xml->endElement();
+				}
+			$xml->endElement();
+		$xml->endElement();
+	$xml->endDocument();
+	$xml->flush();
+}
+
+/**
+ * Create JSON file with all shaarlis
+ */
+function outputJSON() {
+	global $shaarlis_json;
+	global $shaarlis;
+	file_put_contents($shaarlis_json, json_encode($shaarlis));
+}
+
+if(is_file($shaarlis_opml)){
+	// add shaarlis of previous ./out/shaarlis.opml
+	addViaOpml($shaarlis_opml);
+}
+
+$annuaires = array();
+foreach ($annuaires_urls as $annuaires_url) {
+	$f = @file_get_contents($annuaires_url);
+	if(!empty($f)){
+		$json = @json_decode($f, true);
+		if($json){
+			$annuaires = array_merge($annuaires, $json);
+		}
+	}
+}
+if(!empty($annuaires)){
+	foreach ($annuaires as $url => $infos) {
+		if($infos['is_active'] == 1){
+			switch ($infos['type']) {
+				case 'river' :
+					break;
+
+				case 'river-api' :
+					addViaOpml($url."feeds?format=opml");
+					break;
+
+				case 'shaarlo' :
+					addViaOpml($url."opml.php?mod=opml");
+					break;
+
+				case 'shaarlimages' :
+					break;
+
+				case 'shaarli-tv' :
+					break;
+
+				case 'annuaire' :
+					break;
+				
+				default:
+					break;
+			}
+		}
+	}
+	if(!empty($shaarlis)){
+		outputOPML();
+		outputJSON();
+	}
+}
+
+?>
